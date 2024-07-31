f35ddbb02b149c7022499cd9a9ba199149f46a6b
VivekLumbhani <viveklumbhani69@gmail.com>
2024-07-31 12:46:07 +0530

    changes

--- controllers/StaticUserController.php
+++ controllers/StaticUserController.php
@@ -12,15 +12,14 @@ class StaticUserController extends Controller
         $model = new User();
 
         if ($model->load(Yii::$app->request->post())) {
-            // Check if the mobile number already exists
+
             $existingUser = User::findOne(['mobile' => $model->mobile]);
 
             if ($existingUser) {
-                // If mobile number exists, return an error response
+
                 return json_encode(['success' => false, 'errors' => ['mobile' => ['Mobile "' . $model->mobile . '" has already been taken.']]]);
             }
 
-            // Save the model if no duplicate is found
             if ($model->save()) {
                 return json_encode(['success' => true, 'id' => $model->id]);
             } else {
